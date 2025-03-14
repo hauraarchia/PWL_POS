c7be29bd1ff24eaeee731661bfa3c6d58efa312a
haura archia sarapova putri kresna <hauraarchia@gmail.com>
2025-03-14 22:29:22 +0700

    jobsheet4-praktikum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,9 +13,17 @@ class UserController extends Controller
     {
         //tambah data user dengan eloquent model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            // 'level_id' => 2,
+            // 'username' => 'manager_data',
+            // 'nama' => 'Manager 2',
+            // 'password' => Hash::make('12345')
+
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,5 +11,11 @@ class UserModel extends Model
     
     protected $table = 'm_user'; //mendefinisikan nama table yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari table yang digunakan
+
+    // @var array
+
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
+
     
 }
