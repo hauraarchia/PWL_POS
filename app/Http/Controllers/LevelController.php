8f1353628bfc08094023aaa1a6ee8ee37c946193
haura archia sarapova putri kresna <hauraarchia@gmail.com>
2025-05-23 08:10:20 +0700

    js 8 - tugas 1-level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\LevelModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Validator;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Yajra\DataTables\Facades\DataTables;
+
 
 class LevelController extends Controller
 {
@@ -33,10 +35,10 @@ class LevelController extends Controller
     {
         $levels = LevelModel::select('level_id', 'level_kode', 'level_nama');
 
-        if ($request->level_id) {
-            $levels->where('level_id', $request->level_id);
+        $level_id = $request->input('filter_level');
+        if (!empty($level_id)) {
+            $levels->where('level_id', $level_id);
         }
-
         return DataTables::of($levels)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($level) {
@@ -267,5 +269,59 @@ class LevelController extends Controller
         } catch (\Illuminate\Database\QueryException $e) {
             return redirect('/level')->with('error', 'Data level gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
+    }
+    public function import()
+    {
+        return view('level.import');
+    }
+    public function import_ajax(Request $request)
+    {
+        // dd($request->all());
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                // validasi file harus xls atau xlsx, max 1MB
+                'file_level' => ['required', 'mimes:xlsx', 'max:1024']
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+            $file = $request->file('file_level'); // ambil file dari request
+            $reader = IOFactory::createReader('Xlsx'); // load reader file excel
+            $reader->setReadDataOnly(true); // hanya membaca data
+            $spreadsheet = $reader->load($file->getRealPath()); // load file excel
+            $sheet = $spreadsheet->getActiveSheet(); // ambil sheet yang aktif
+            $data = $sheet->toArray(null, false, true, true); // ambil data excel
+            $insert = [];
+            if (count($data) > 1) { // jika data lebih dari 1 baris
+                foreach ($data as $baris => $value) {
+                    if ($baris > 1) { // baris ke 1 adalah header, maka lewati
+                        $insert[] = [
+                            'level_kode' => $value['A'],
+                            'level_nama' => $value['B'],
+                            'created_at' => now(),
+                        ];
+                    }
+                }
+                if (count($insert) > 0) {
+                    // insert data ke database, jika data sudah ada, maka diabaikan
+                    LevelModel::insertOrIgnore($insert);
+                }
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diimport'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Tidak ada data yang diimport'
+                ]);
+            }
+        }
+        return redirect('/');
     }
 }
--- resources/views/level/index.blade.php
+++ resources/views/level/index.blade.php
@@ -1,104 +1,118 @@
 @extends('layouts.template')
-
 @section('content')
-    <div class="card card-outline card-primary">
+    <div class="card">
         <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
+            <h3 class="card-title">Daftar Level</h3>
             <div class="card-tools">
-                <a href="{{ url('level/create') }}" class="btn btn-sm btn-primary mt-1">Tambah</a>
-                <button onclick="modalAction('{{ url('level/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+
+                <button onclick="modalAction('{{ url('/level/import') }}')" class="btn btn-info">Import Level</button>
+                <a href="{{ url('/level/create') }}" class="btn btn-primary">Tambah Data</a>
+                <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
             </div>
         </div>
         <div class="card-body">
+            <!-- untuk Filter data -->
+            <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group form-group-sm row text-sm mb-0">
+                            <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
+                            <div class="col-md-3">
+                                <select name="filter_level" class="form-control form-control-sm filter_level">
+                                    <option value="">- Semua -</option>
+                                    @foreach ($level as $l)
+                                        <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Level Pengguna</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <select name="level_id" id="level_id" class="form-control" required>
-                                <option value="">- Semua -</option>
-                                @foreach ($level as $item)
-                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Level Pengguna</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+            <table class="table table-bordered table-sm table-striped table-hover" id="table-level">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Level Kode</th>
-                        <th>Level Nama</th>
-                        <th>aksi</th>
+                        <th>No</th>
+                        <th>Kode level</th>
+                        <th>Nama level</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
+                <tbody></tbody>
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" 
-        data- backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true">
-</div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false"
+        data-width="75%" aria-hidden="true"></div>
 @endsection
-
-@push('css')
-@endpush
-
 @push('js')
     <script>
-        function modalAction(url = ''){
-            $('#myModal').load(url,function(){
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
                 $('#myModal').modal('show');
             });
         }
+        var tableLevel;
         $(document).ready(function() {
-            var dataLevel = $('#table_level').DataTable({
+            tableLevel = $('#table-level').DataTable({
+                processing: true,
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('level/list') }}",
                     "dataType": "json",
                     "type": "POST",
                     "data": function(d) {
-                        d.level_id = $('#level_id').val();
+                        d.filter_level = $('.filter_level').val();
                     }
                 },
                 columns: [{
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "level_kode",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "level_nama",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
+                        data: null,
+                        className: "text-center",
+                        width: "5%",
+                        orderable: false,
+                        searchable: false,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        data: "level_kode",
+                        className: "",
+                        width: "10%",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "level_nama",
+                        className: "",
+                        width: "37%",
+                        orderable: true,
+                        searchable: true,
+                    },
+                    {
+                        data: "aksi",
+                        className: "text-center",
+                        width: "14%",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+            $('#table-level_filter input').unbind().bind().on('keyup', function(e) {
+                if (e.keyCode == 13) { // enter key
+                    tableLevel.search(this.value).draw();
                 }
-            ]
             });
-            $('#level_id').on('change', function() {
-                dataLevel.ajax.reload();
+            $('.filter_level').change(function() {
+                tableLevel.draw();
             });
-        })
+        });
     </script>
 @endpush
